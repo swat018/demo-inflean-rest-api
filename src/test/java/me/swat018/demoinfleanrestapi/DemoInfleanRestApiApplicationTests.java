2c29ec053fe531d05699d6c297bc76c73b6c73b5
jinwoo park <swat018@gmail.com>
2022-03-21 23:51:37 +0900

    Initial commit

---
+++ src/test/java/me/swat018/demoinfleanrestapi/DemoInfleanRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.swat018.demoinfleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInfleanRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
