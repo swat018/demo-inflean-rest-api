ebe52f524dc855bfc3a190842d33f619d8d81aae
jinwoo park <swat018@gmail.com>
2022-03-22 19:38:45 +0900

    feat : Event 생성 API 구현: Event 도메인 구현

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - REST API 및 프로젝트 소개
   -
   - Project 만들기
+  - Event 생성 API 구현: Event 도메인 구현
---
+++ src/main/java/me/swat018/demoinfleanrestapi/EventStatus.java
@@ -0,0 +1,6 @@
+package me.swat018.demoinfleanrestapi;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMEND, CLOSED_ENROLLMENT, STARTED, ENDED
+}
