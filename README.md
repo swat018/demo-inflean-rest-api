1e09b6565a3d0f0cc3265929b469e21bcb87e6fd
jinwoo park <swat018@gmail.com>
2022-03-26 12:34:34 +0900

    feat : PostgreSQL 적용 - 테스트용 DB와 설정 분리하기

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
   - 스프링 REST Docs 적용
   - 스프링 REST Docs: 링크, (Req, Res) 필드와 헤더 문서화 (각종 문서 조각 생성하기)
   - 스프링 REST Docs 문서 빌드
+  - PostgreSQL 적용 - 테스트 DB와 설정 분리하기
--- src/test/java/me/swat018/demoinfleanrestapi/events/EventControllerTests.java
+++ src/test/java/me/swat018/demoinfleanrestapi/events/EventControllerTests.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 public class EventControllerTests {
 
     @Autowired
